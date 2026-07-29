ea3495e195a1239a04bb8f970dc6f162644d97fb
Trupti Mahesh <truptimp02@gmail.com>
2026-07-29 10:36:30 +0530

    Update print statements in citakhil.py

--- lilly.py
+++ lilly.py
@@ -1,6 +1,6 @@
 def multiply(a, b):
     return a * b
 
-a=int(input("Enter a:")
-b=int(input("Enter b:")
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
 print(f"product of {a} and {b} is {multiply(a, b)}")
